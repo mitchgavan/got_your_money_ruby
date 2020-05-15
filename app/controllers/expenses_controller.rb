2e0aaebeeee35a346716705450dc6a8c1f0b93d5
Mitch Gavan <mitchgavan@gmail.com>
2020-05-15 22:03:43 +1000

    feat: create new expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,20 @@
 class ExpensesController < ApplicationController
   def new
   end
+
+  def create
+    @expense = Expense.new(expense_params)
+
+    @expense.save
+    redirect_to @expense
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
+  private
+    def expense_params
+      params.require(:expense).permit(:title, :cost, :date)
+    end
 end
