81a7cb8eeec4e84b81c843fcfaac0ecc2f158e28
Mitch Gavan <mitchgavan@gmail.com>
2020-05-22 18:41:50 +1000

    feat: purge css in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./app/views/**/*.html.erb"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -10,5 +15,6 @@ module.exports = {
       },
       stage: 3
     }),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 }
